cc76b0c2936bbeaec6868c4ced550130ef066b76
SketchBuch <sb.design.de@gmail.com>
2020-03-09 21:39:11 +0100

    chore: updates

--- src/server/templates/default.ts
+++ src/server/templates/default.ts
@@ -3,11 +3,12 @@ const defaultTemplate = `
   <html>
     <head>
       <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta
         name="viewport"
         content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"
       />
-      <title>App</title>
+      <title>React Parcel Express Typescript</title>
       {styles}
     </head>
 
